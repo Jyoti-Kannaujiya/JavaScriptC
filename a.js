8af42a7aced65a28e368e6e9dff36eb78a0ee4cd
jyoti kannaujiya <jyoti.blitzshipz@gmail.com>
2026-03-09 15:33:22 +0530

    abc

---
+++ a.js
@@ -0,0 +1,9 @@
+console.log("hdhhdhd");
+const accountId=123345
+let accountEmail="jyoti122@google.com"
+var accountPassword=12121
+accountCity="Jaipur"
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+accountEmail="guewfe"
+accountPassword=565
+console.log(accountEmail,accountPassword);
